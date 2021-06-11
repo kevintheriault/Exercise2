22cce60bcbd2dd50200a0e85f74af921eb6737c0
kevintheriault <77777560+kevintheriault@users.noreply.github.com>
2021-06-11 19:01:40 -0400

    Created customer data service and customer class with param constructor.

---
+++ src/main/java/sheridan/theriake/exercise2/database/CustomerDataService.java
@@ -0,0 +1,9 @@
+package sheridan.theriake.exercise2.database;
+
+import sheridan.theriake.exercise2.domain.Customer;
+
+import java.util.List;
+
+public interface CustomerDataService {
+    List<Customer> getAllCustomers();
+}
---
+++ src/main/java/sheridan/theriake/exercise2/domain/Customer.java
@@ -0,0 +1,95 @@
+package sheridan.theriake.exercise2.domain;
+
+import java.io.Serializable;
+
+public class Customer implements Serializable {
+
+    private String customerId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+
+    public Customer(){
+
+    }
+
+    public Customer(String customerId, String firstName, String lastName, String email, String street,
+                    String city, String state, String zipCode) {
+        this.customerId = customerId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+}
