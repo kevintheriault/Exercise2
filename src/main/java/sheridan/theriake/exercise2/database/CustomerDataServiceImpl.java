4b35e099d8d138d328e253e2f6d4c6e71e9f6078
kevintheriault <77777560+kevintheriault@users.noreply.github.com>
2021-06-11 19:09:54 -0400

    Implemented data service interface.

---
+++ src/main/java/sheridan/theriake/exercise2/database/CustomerDataServiceImpl.java
@@ -0,0 +1,38 @@
+package sheridan.theriake.exercise2.database;
+
+import sheridan.theriake.exercise2.domain.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerDataServiceImpl implements CustomerDataService{
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerDataServiceImpl(CustomerRepository customerRepository){
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public List<Customer> getAllCustomers(){
+
+        List<CustomerEntity> entities = customerRepository.findAll();
+        List<Customer> customers = new ArrayList<Customer>();
+        for(CustomerEntity entity: entities){
+            customers.add(
+                    new Customer(
+                            entity.toString(),
+                            entity.getFirstName(),
+                            entity.getLastName(),
+                            entity.getEmail(),
+                            entity.getStreet(),
+                            entity.getCity(),
+                            entity.getState(),
+                            entity.getZipCode()
+                    )
+            );
+        }
+
+        return customers;
+        }
+    }
